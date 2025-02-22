ffb7b36677c8c3ec194e08c8c25a8e23ede3ec06
SergioAbadOrdonez <sergioabaddaw@gmail.com>
2025-02-22 12:37:09 +0100

    Expection Class updated

--- src/fullTeamException.java
+++ src/fullTeamException.java
@@ -1,8 +1,5 @@
 public class fullTeamException extends Exception {
-    {
-        public fullTeamException(String message)
-        {
-            super(message)
-        }
+    public fullTeamException(String message) {
+        super(message);
     }
 }
