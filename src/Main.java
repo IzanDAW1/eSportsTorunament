06f979ae0e5efc7c4d7c54e55ba7ec91137077f0
Izan <izajimmoy@alu.edu.gva.es>
2025-02-22 12:39:41 +0100

    Method Options is now implemented on class Main

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,42 @@ public class Main {
         return sc.nextLine();
     }
 
+    public static void Options(String option)
+    {
+        switch (option)
+        {
+            case "1":
+
+                break;
+            case "2":
+
+                break;
+            case "3":
+
+                break;
+            case "4":
+
+                break;
+            case "5":
+
+                break;
+            case "6":
+
+                break;
+            case "7":
+
+                break;
+            case "8":
+
+                break;
+            case "9":
+
+                break;
+            case "10":
+                System.out.println("Leaving...");
+                break;
+        }
+    }
 
 
     public static void main(String[] args) {
