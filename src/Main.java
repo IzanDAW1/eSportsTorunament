2945f3815548947bc7a893a294030a61a58dea2d
Alejandro <alejandrogonzalezaldeguer@gmail.com>
2025-02-22 11:30:18 +0100

    commit alejandro

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is a test to prove that github is vinculated correctly :)");
+
+        System.out.println("commit alejandro");
     }
 }
