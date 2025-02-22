93e4147df04eae7cd511340fe7b9d7bb3e304c0e
Izan <izajimmoy@alu.edu.gva.es>
2025-02-22 12:15:01 +0100

    Subclass Player updated with the attributes,constructor,inherited with Participant and getters and setters created.

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,32 @@
-public class Player{
+import java.util.Random;
 
+public class Player extends Participant{
+    int level;
+    float ranking;
+
+    public Player(String name,int level,float ranking)
+    {
+        super(name);
+        Random r = new Random();
+        this.level= r.nextInt(1,101);
+        this.ranking=ranking;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+    public float getRanking()
+    {
+        return ranking;
+    }
+
+    public void setLevel(int level)
+    {
+        this.level=level;
+    }
+    public void setRanking(float ranking)
+    {
+        this.ranking=ranking;
+    }
 }
