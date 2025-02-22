c714455d02d30057f5dae6e95dde02fa469c1dff
SergioAbadOrdonez <sergioabaddaw@gmail.com>
2025-02-22 12:35:30 +0100

    Class Team finished

--- src/Team.java
+++ src/Team.java
@@ -1,2 +1,20 @@
-public class Team{
+public class Team extends Participant{
+    Player[] players = new Player[5];
+    int countOfPlayers;
+
+    public Team(String name,Player[] players) {
+        super(name);
+        this.players=players;
+        countOfPlayers=0;
+    }
+
+    public void AddPlayer(Player p){
+        if(countOfPlayers>5){
+            throw new fullTeamException("Reached the max ammount of players in a team. Sorry");
+        }
+        else{
+            players[countOfPlayers]=p;
+            countOfPlayers++;
+        }
+    }
 }
